b1eab00fc9b8b45bac49a4bd2832ff8115cddb09
juliet26 <36687161+juliet26@users.noreply.github.com>
2018-04-11 20:43:45 -0500

    Calcular propina

--- AppPropina/AppPropina/AppPropina/MainPage.xaml.cs
+++ AppPropina/AppPropina/AppPropina/MainPage.xaml.cs
@@ -12,6 +12,19 @@ namespace AppPropina
 		public MainPage()
 		{
 			InitializeComponent();
+            btnCalcular.Clicked += BtnCalcular_Clicked; 
 		}
-	}
+
+        private void BtnCalcular_Clicked(object sender, EventArgs e)
+        {
+            var total = decimal.Parse(entTotal.Text);
+            var propina = decimal.Parse(entPropina.Text);
+            var numPersonas = int.Parse(entPersonas.Text);
+            var totalPropina = ((total * propina) / 100);
+            txtTotalPropina.Detail = totalPropina.ToString("C");
+            txtPropinaPersona.Detail = (totalPropina/ numPersonas).ToString("C");
+            txtTotalPersona.Detail = ((total + totalPropina) / numPersonas).ToString("C");
+            txtTotal.Detail = (total + totalPropina).ToString("C");
+        }
+    }
 }
